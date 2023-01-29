eb8fd6d2fc91af9a8318de6404fd32e436eb4299
Tembulat Tokmakov <tembatokmakov43@gmail.com>
2023-01-29 17:08:54 +0300

    second commit

--- src/Main.java
+++ src/Main.java
@@ -5,7 +5,9 @@ public class Main {
     public static void main(String[] args) {
         int sum = 3587;
         int cash = 100;
-        System.out.println("Кол-во бонусов " + sum / 100);
-        System.out.println("Сумма с учетом бонусов " + (cash + sum + sum / 100));
+        int bonus = 0;
+        if (sum >= 1000) bonus = sum / 100;
+        System.out.println("Кол-во бонусов " + bonus);
+        System.out.println("Сумма с учетом бонусов " + (cash + sum + bonus));
     }
 }
