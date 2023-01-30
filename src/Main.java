e035c71bbaccabf7e5c824e631cd5e0c7f04aa80
Tembulat Tokmakov <tembatokmakov43@gmail.com>
2023-01-30 15:56:06 +0300

    Commit

--- src/Main.java
+++ src/Main.java
@@ -3,10 +3,12 @@ public class Main {
     }
 
     public static void main(String[] args) {
-        int sum = 3587;
+        int sum = 3967;
         int cash = 100;
         int bonus = 0;
-        if (sum >= 1000) bonus = sum / 100;
+        if (sum > 1000) {
+            bonus = sum / 100;
+        }
         System.out.println("Кол-во бонусов " + bonus);
         System.out.println("Сумма с учетом бонусов " + (cash + sum + bonus));
     }
